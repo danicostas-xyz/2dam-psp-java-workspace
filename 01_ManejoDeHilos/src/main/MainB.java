0cfc713e0095a5b2a403d2fc4de7132bddef31c1
Dani Costas <145155375+danicostas-xyz@users.noreply.github.com>
2024-11-14 13:22:36 +0100

    Update MainB in 01_ManejoDeHilos. Add an implementation of Runnable using an anonymous lambda function as a parameter in the Thread constructor

--- 01_ManejoDeHilos/src/main/MainB.java
+++ 01_ManejoDeHilos/src/main/MainB.java
@@ -17,8 +17,9 @@ public class MainB {
 		String cadena = sc.nextLine();
 		sc.close();
 
-		/* 1. PODEMOS IMPLEMENTAR LA INTERFAZ RUNNABLE DIRECTAMENTE EN UNA CLASE ABSTRACTA
-		 * Runnable r = new Runnable() {
+		/*
+		 * 1. PODEMOS IMPLEMENTAR LA INTERFAZ RUNNABLE DIRECTAMENTE EN UNA CLASE
+		 * ABSTRACTA Runnable r = new Runnable() {
 		 * 
 		 * @Override public void run() { // TODO Auto-generated method stub
 		 * 
@@ -27,13 +28,48 @@ public class MainB {
 		 * };
 		 */
 
-		/* 2. PODEMOS IMPLEMENTAR LA INTERFAZ EN UNA CLASE (PRIMOS RUNNABLE) Y LUEGO CREAR EL OBJETO*/
+		/*
+		 * 2. PODEMOS IMPLEMENTAR LA INTERFAZ EN UNA CLASE (PRIMOS RUNNABLE) Y LUEGO
+		 * CREAR EL OBJETO
+		 */
 		PrimosRunnable r1 = new PrimosRunnable(n1);
 		UnoAMilMillones hilo2 = new UnoAMilMillones(n2);
-		
-		/* 3. PODEMOS IMPLEMENTAR DIRECTAMENTE LA INTERFAZ RUNNABLE CON UNA FUNCIÓN LAMBDA*/
-		Runnable r2 = () -> {
 
+		/*
+		 * 3. PODEMOS IMPLEMENTAR DIRECTAMENTE LA INTERFAZ RUNNABLE CON UNA FUNCIÓN
+		 * LAMBDA
+		 */
+//		Runnable r2 = () -> {
+//
+//			String nombreHilo = Thread.currentThread().getName();
+//			System.out.println("---------------------------------------------------------");
+//			System.out.println("Inicio hilo " + nombreHilo);
+//
+//			long inicio = System.currentTimeMillis();
+//
+//			long repeticiones = n1 - n2;
+//			if (repeticiones < 0)
+//				repeticiones *= -1; // Si el número es negativo, se convierta en positivo
+//
+//			for (long i = 0; i < repeticiones; i++) {
+//				System.out.println(cadena);
+//			}
+//
+//			long fin = (System.currentTimeMillis() - inicio);
+//
+//			System.out.println("Tiempo procesamiento " + nombreHilo + ": " + fin + " milisegundos.");
+//			System.out.println("---------------------------------------------------------");
+//		};
+
+		Thread hilo1 = new Thread(r1);
+
+		/*
+		 * 4. Podemos implementar la interfaz runnable directamente dentro de los
+		 * paréntesis del new Thread(). En este caso estamos haciendo lo mismo que en el
+		 * punto 3, pero en lugar de crearnos una referencia para implementar Runnable,
+		 * creamos la lambda directamente como parámetro, de manera "anónima"
+		 */
+		Thread hilo3 = new Thread(() -> {
 			String nombreHilo = Thread.currentThread().getName();
 			System.out.println("---------------------------------------------------------");
 			System.out.println("Inicio hilo " + nombreHilo);
@@ -52,10 +88,7 @@ public class MainB {
 
 			System.out.println("Tiempo procesamiento " + nombreHilo + ": " + fin + " milisegundos.");
 			System.out.println("---------------------------------------------------------");
-		};
-
-		Thread hilo1 = new Thread(r1);
-		Thread hilo3 = new Thread(r2);
+		});
 
 		hilo1.start();
 		hilo2.start();
